df4652d6f0267948c418c88ac744c4fc9d25226c
JFronny <git@akbvopenfl1.hopto.org>
2022-12-31 20:53:28 +0100

    include natives in build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import org.gradle.internal.os.OperatingSystem
-import java.nio.file.Path
-
 plugins {
     java
     application
     id("org.beryx.jlink") version "2.26.0"
+    id("de.undercouch.download") version "5.3.0"
 }
 
 group = "io.gitlab.jfronny"
@@ -18,6 +16,7 @@ repositories {
     mavenCentral()
     // Mirror for javagi maven, since that requires authentication
     // and I didn't want to figure out how to pass the GitHub Actions token here
+    // Also, the build hosted here includes the libload branches changes, which are needed for windows support
     maven("https://maven.frohnmeyer-wds.de/java-gi")
 }
 
@@ -29,6 +28,8 @@ dependencies {
 tasks.compileJava.get().options.compilerArgs.add("--enable-preview")
 tasks.run.get().jvmArgs!!.add("--enable-preview")
 
+val os = org.gradle.internal.os.OperatingSystem.current()!!
+
 jlink {
     addOptions("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages", "--verbose")
     launcher {
@@ -38,9 +39,9 @@ jlink {
         vendor = "Some Corp"
         jvmArgs.add("--enable-preview")
         installerName = "JavaGI Multiplatform Example"
-        if(OperatingSystem.current().isMacOsX) {
+        if(os.isMacOsX) {
             //installerType = "app-image"
-        } else if(OperatingSystem.current().isWindows) {
+        } else if(os.isWindows) {
             //installerType = "msi"
             installerOptions.addAll(listOf("--win-per-user-install", "--win-dir-chooser", "--win-menu"))
         } else {
@@ -49,17 +50,27 @@ jlink {
     }
 }
 
-//TODO download the Windows natives if running on Windows
-//val pth = projectDir.resolve("natives")
-//tasks.jpackageImage {
-//    doLast {
-//        def appName = jpackageData . imageName
-//                def dir = "$jpackageData.imageOutputDir/$appName"
-//        file("$dir/lib").mkdirs()
-//        ant.move(todir: "$dir/lib") {
-//            fileset(dir: dir) {
-//                include name : "*.dll"
-//            }
-//        }
-//    }
-//}
+if (os.isWindows) {
+    val downloadNatives by tasks.registering(de.undercouch.gradle.tasks.download.Download::class) {
+        src("https://github.com/JFronny/javagi-multiplatform/releases/download/natives/natives.zip")
+        dest(buildDir.resolve("natives.zip"))
+        overwrite(false)
+    }
+
+    val extractNatives by tasks.registering(Copy::class) {
+        dependsOn(downloadNatives)
+        from(zipTree(downloadNatives.get().dest))
+        into(buildDir.resolve("natives"))
+    }
+
+    tasks.jpackageImage {
+        dependsOn(extractNatives)
+        doLast {
+            val dir = jpackageData.imageOutputDir.resolve(jpackageData.imageName).resolve("lib")
+            dir.mkdirs()
+            for (file in extractNatives.get().destinationDir.listFiles()) {
+                file.copyTo(dir.resolve(file.name))
+            }
+        }
+    }
+}
