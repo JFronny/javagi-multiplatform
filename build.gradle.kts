818c0cc14ea35b7b04e6fe7bf806c82b83affd79
JFronny <git@akbvopenfl1.hopto.org>
2023-01-01 13:31:43 +0100

    Try to fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,26 +30,6 @@ tasks.run.get().jvmArgs!!.add("--enable-preview")
 
 val os = org.gradle.internal.os.OperatingSystem.current()!!
 
-jlink {
-    addOptions("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages", "--verbose")
-    launcher {
-        name = "javagi-multiplatform"
-    }
-    jpackage {
-        vendor = "Some Corp"
-        jvmArgs.addAll(listOf("--enable-preview", "-Djava.library.path=\$APPDIR"))
-        installerName = "JavaGI Multiplatform Example"
-        if(os.isMacOsX) {
-            //installerType = "app-image"
-        } else if(os.isWindows) {
-            //installerType = "msi"
-            installerOptions.addAll(listOf("--win-per-user-install", "--win-dir-chooser", "--win-menu"))
-        } else {
-            //installerType = "deb"
-        }
-    }
-}
-
 if (os.isWindows) {
     val downloadNatives by tasks.registering(de.undercouch.gradle.tasks.download.Download::class) {
         src("https://github.com/JFronny/javagi-multiplatform/releases/download/natives/natives.zip")
@@ -65,12 +45,29 @@ if (os.isWindows) {
 
     tasks.jpackageImage {
         dependsOn(extractNatives)
-        doLast {
-            val dir = jpackageData.imageOutputDir.resolve(jpackageData.imageName).resolve("app")
-            dir.mkdirs()
-            for (file in extractNatives.get().destinationDir.listFiles()) {
-                file.copyTo(dir.resolve(file.name))
-            }
+    }
+}
+
+jlink {
+    addOptions("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages", "--verbose")
+    launcher {
+        name = "javagi-multiplatform"
+    }
+    jpackage {
+        vendor = "Some Corp"
+        jvmArgs.add("--enable-preview")
+        installerName = "JavaGI Multiplatform Example"
+        if (os.isWindows) {
+            jvmArgs.add("-Djava.library.path=\$APPDIR/natives")
+            imageOptions.addAll(listOf("--app-content", buildDir.resolve("natives").toString()))
+        }
+        if(os.isMacOsX) {
+            //installerType = "app-image"
+        } else if(os.isWindows) {
+            //installerType = "msi"
+            installerOptions.addAll(listOf("--win-per-user-install", "--win-dir-chooser", "--win-menu"))
+        } else {
+            //installerType = "deb"
         }
     }
 }
